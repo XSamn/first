7ec1a53a76d80e518b8c06f5f2e893f6a01c050b
XSamn <128686045+XSamn@users.noreply.github.com>
2023-04-08 14:40:27 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -6,6 +6,12 @@ var phone = document.querySelector("#phone");
 var ul = document.querySelector("ul");
 var input = document.querySelector("input");
 var ss = document.querySelector("#ss_input");
+var today = new Date();
+var y = today.getFullYear();
+var M = today.getMonth() + 1;
+var d = today.getDate();
+var h = today.getHours();
+var m = today.getMinutes();
 
 
 btn.onclick = function () {
@@ -16,60 +22,61 @@ btn.onclick = function () {
         var li = document.createElement("li");
         var i;
         var txt = '';
-        var ss=document.forms[0].ss;
+        var ss = document.forms[0].ss;
         for (i = 0; i < ss.length; i++) {
             if (ss[i].checked) {
                 txt = txt + ss[i].value + " ";
-            }}
-            //将文本框中的内容放到li中,a标签中href中的内容代表什么都不执行
-            li.innerHTML = name1.value + "   " + "感觉" + txt + "<br>" + "<br>" + text.value + "<br>" + "<a href='javascript:;'>删除</a>"
+            }
+        }
+        //将文本框中的内容放到li中,a标签中href中的内容代表什么都不执行
+        li.innerHTML = name1.value + "   " + "感觉" + txt + "<br>" + "<br>" + text.value + "<br>" + "<br>" + y + '年' + M + '月' + d + '日' + "  " + h + ":" + m  + "<a href='javascript:;'>删除</a>"
 
 
-            text.value = "";
-            name1.value = "";
-            phone.value = "";
-            //添加之后文本框的内容清空
-            //将留言添加到ul中，而且在最上方
-            ul.insertBefore(li, ul.children[0]);
-            //当点击删除时删除ul中的li
-            var as = document.querySelectorAll("a");
-            //循环给每个删除绑定事件
-            for (var i = 0; i < as.length; i++) {
-                as[i].onclick = function () {
-                    //删除当前a标签所在位置的父节点li
-                    ul.removeChild(this.parentNode);
-                }
+        text.value = "";
+        name1.value = "";
+        phone.value = "";
+        //添加之后文本框的内容清空
+        //将留言添加到ul中，而且在最上方
+        ul.insertBefore(li, ul.children[0]);
+        //当点击删除时删除ul中的li
+        var as = document.querySelectorAll("a");
+        //循环给每个删除绑定事件
+        for (var i = 0; i < as.length; i++) {
+            as[i].onclick = function () {
+                //删除当前a标签所在位置的父节点li
+                ul.removeChild(this.parentNode);
             }
         }
     }
+}
 
-    const inputs = document.querySelectorAll(".input");
+const inputs = document.querySelectorAll(".input");
 
-    function focusFunc() {
-        let parent = this.parentNode;
-        parent.classList.add("focus");
+function focusFunc() {
+    let parent = this.parentNode;
+    parent.classList.add("focus");
+}
+
+function blurFunc() {
+    let parent = this.parentNode;
+    if (this.value == "") {
+        parent.classList.remove("focus")
+    }
+}
+inputs.forEach((input) => {
+    input.addEventListener("focus", focusFunc);
+    input.addEventListener("blur", blurFunc);
+})
+
+function choose(obj) {
+    var ss = document.getElementsByName("ss");
+    var i;
+    var txt = '';
+    for (var i = 0; i < ss.length; i++) {
+        ss[i].checked = false;
+
     }
 
-    function blurFunc() {
-        let parent = this.parentNode;
-        if (this.value == "") {
-            parent.classList.remove("focus")
-        }
-    }
-    inputs.forEach((input) => {
-        input.addEventListener("focus", focusFunc);
-        input.addEventListener("blur", blurFunc);
-    })
+    obj.checked = true;
 
-    function choose(obj) {
-        var ss = document.getElementsByName("ss");
-        var i;
-        var txt = '';
-        for (var i = 0; i < ss.length; i++) {
-            ss[i].checked = false;
-
-        }
-
-        obj.checked = true;
-
-    }
+}
